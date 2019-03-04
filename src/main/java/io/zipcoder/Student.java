4a7e330b747780aa164df29eb675f1d12812c51b
Charles Wilmer <charleswilmer@zipcoders-MacBook-Pro.local>
2019-03-03 19:52:43 -0500

    made changes to student class, stopped to study for assesment 2

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,44 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    private String firstName;
+    private  String lastName;
+    public ArrayList<Double> examScores;
+
+        public Student (String firstName, String lastName, ArrayList<Double>testScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<Double>(testScores);
+
+
+
+        }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setExamScores(ArrayList<Double> examScores) {
+        this.examScores = examScores;
+    }
+
+    public String getNumberofTestTaken(){
+
+
+    }
 }
